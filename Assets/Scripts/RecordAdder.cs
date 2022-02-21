bce602af80f078c9fbc941630eba7e87a4639974
Varga András Bendegúz <vargaandras@gszi.edu.hu>
2022-02-21 09:25:53 +0100

    Started working on adding and modifying questions

--- Assets/Scripts/RecordAdder.cs
+++ Assets/Scripts/RecordAdder.cs
@@ -7,11 +7,22 @@ public class RecordAdder : MonoBehaviour
 {
 
     [SerializeField] private DatabaseManager databaseManager;
-    [SerializeField] private InputField[] quesionInputs = new InputField[6];
+    [SerializeField] private Dropdown difficultyInput;
+    [SerializeField] private InputField[] quesionInputs = new InputField[5];
 
-    public void AddQuestion()
+    public void AddQuiz()
     {
-        databaseManager.RunQuery("INSERT INTO questions (difficulty, question, good_answer, bad_answer1, bad_answer2, bad_answer3, user_added) VALUES (" + quesionInputs[0].textComponent.text + ", '" + quesionInputs[1].textComponent.text + "', '" + quesionInputs[2].textComponent.text + "', '" + quesionInputs[3].textComponent.text + "', '" + quesionInputs[4].textComponent.text + "', '" + quesionInputs[5].textComponent.text + ", 1"+ "')");
+        QuizHandler.QuizData data = new QuizHandler.QuizData();
+
+        data.difficulty = difficultyInput.value;
+        data.question = quesionInputs[0].text;
+        data.good_answer = quesionInputs[1].text;
+        for (int i = 0; i < 3; i++)
+        {
+            data.bad_answers[i] = quesionInputs[i + 2].text;
+        }
+
+        
         foreach (InputField field in quesionInputs)
         {
             field.text = "";
